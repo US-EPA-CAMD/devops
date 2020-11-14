3dcec91a3e72f030166b8c48aba42b9539793b22
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-11-14 14:44:42 -0600

    changing env var to test w/epa-prototyping org

--- scripts/environment-variables.sh
+++ scripts/environment-variables.sh
@@ -2,15 +2,15 @@
 
 # GLOBAL
 echo "CF_API_URL=https://api.fr.cloud.gov" >> $GITHUB_ENV
-echo "CF_ORG_NAME=epa-easey" >> $GITHUB_ENV
+echo "CF_ORG_NAME=epa-prototyping" >> $GITHUB_ENV
 echo "CF_ARTIFACTS_SVC=deployment-artifacts" >> $GITHUB_ENV
 echo "CF_ARTIFACTS_SVC_KEY=deployment-artifacts-svc-key" >> $GITHUB_ENV
 
 # DEV
-echo "CF_SPACE_NAME=dev" >> $GITHUB_ENV
+echo "CF_SPACE_NAME=dev-easey-in" >> $GITHUB_ENV
 
 # TEST
-echo "CF_SPACE_NAME=test" >> $GITHUB_ENV
+#echo "CF_SPACE_NAME=test" >> $GITHUB_ENV
 
 # STAGING
-echo "CF_SPACE_NAME=staging" >> $GITHUB_ENV
+#echo "CF_SPACE_NAME=staging" >> $GITHUB_ENV
