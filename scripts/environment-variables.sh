106dcf3b104e0151d3079ec246bc371cc177135b
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-11-16 15:03:16 -0600

    changed -eq to = for string comparison in env var script

--- scripts/environment-variables.sh
+++ scripts/environment-variables.sh
@@ -10,19 +10,19 @@ echo "CF_ARTIFACTS_SVC_KEY=deployment-artifacts-svc-key" >> $GITHUB_ENV
 echo "CF_ARTIFACTS_SVC_SPACE=test" >> $GITHUB_ENV
 
 # DEV
-if [ $ENV -e "dev" ]
+if [ $ENV = "dev" ]
 then
   echo "CF_SPACE_NAME=dev" >> $GITHUB_ENV
 fi
 
 # TEST
-if [ $ENV -e "tst" ]
+if [ $ENV = "tst" ]
 then
   echo "CF_SPACE_NAME=test" >> $GITHUB_ENV
 fi
 
 # STAGING
-if [ $ENV -e "stg" ]
+if [ $ENV = "stg" ]
 then
   echo "CF_SPACE_NAME=staging" >> $GITHUB_ENV
 fi
