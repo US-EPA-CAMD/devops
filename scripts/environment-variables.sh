81c4a1603d310c783276c742ccd91a218c171875
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-11-16 14:50:23 -0600

    added conditionals around env var and added cloud.gov env setup file

--- scripts/environment-variables.sh
+++ scripts/environment-variables.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# GLOBAL
+# CLOUD.GOV
 echo "CF_API_URL=https://api.fr.cloud.gov" >> $GITHUB_ENV
 echo "CF_ORG_NAME=epa-easey" >> $GITHUB_ENV
+
+# DEPLOYMENT ARTIFACTS S3 STORAGE
 echo "CF_ARTIFACTS_SVC=deployment-artifacts" >> $GITHUB_ENV
 echo "CF_ARTIFACTS_SVC_KEY=deployment-artifacts-svc-key" >> $GITHUB_ENV
+echo "CF_ARTIFACTS_SVC_SPACE=test" >> $GITHUB_ENV
 
 # DEV
-echo "CF_SPACE_NAME=dev" >> $GITHUB_ENV
+if [ $ENV -e "dev" ]
+then
+  echo "CF_SPACE_NAME=dev" >> $GITHUB_ENV
+fi
 
 # TEST
-#echo "CF_SPACE_NAME=test" >> $GITHUB_ENV
+if [ $ENV -e "tst" ]
+then
+  echo "CF_SPACE_NAME=test" >> $GITHUB_ENV
+fi
 
 # STAGING
-#echo "CF_SPACE_NAME=staging" >> $GITHUB_ENV
+if [ $ENV -e "stg" ]
+then
+  echo "CF_SPACE_NAME=staging" >> $GITHUB_ENV
+fi
