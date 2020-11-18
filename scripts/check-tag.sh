5b62d06015c5be8141e4c14e874623193eb153fa
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2020-11-18 14:15:56 -0600

    forgot to save changes to check-tag script before commiting

--- scripts/check-tag.sh
+++ scripts/check-tag.sh
@@ -7,15 +7,13 @@ echo "Checking for tag..."
 tag=$(git tag --points-at HEAD)
 
 case $tag in
-  # change this to pattern match tag format of tst-[0-9].[0-9].[0-9]
   (tst-[0-9]*.[0-9]*.[0-9]*)
     echo "CF_ORG_SPACE=test" >> $GITHUB_ENV
     ;;
-  # change this to pattern match tag format of stg-[0-9].[0-9].[0-9]
   (stg-[0-9]*.[0-9]*.[0-9]*)
     echo "CF_ORG_SPACE=staging" >> $GITHUB_ENV
     ;;
-  # This is to test no tag for dev env as we do not want a malformed tag pushing to dev
+  # This is to match case of no tag for dev env as we do not want a malformed tag pushing to dev
   "")
     echo "CF_ORG_SPACE=dev" >> $GITHUB_ENV
     ;;
