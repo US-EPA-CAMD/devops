fa65ef2a0fc0cb07f357ee4c66416c0edf647f1d
Srinivasa Regeti <regeti@regeti.com>
2020-12-07 01:25:04 -0500

    Added rollback tags

--- scripts/check-tag.sh
+++ scripts/check-tag.sh
@@ -18,6 +18,14 @@ case $tag in
     echo "CF_ORG_SPACE=test" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=test"
     ;;
+  (rbk-tst-v[0-9]*.[0-9]*)
+    echo "CF_ORG_SPACE=test" >> $GITHUB_ENV
+    echo "CF_ORG_SPACE=test"
+    ;;
+  (rbk-stg-v[0-9]*.[0-9]*)
+    echo "CF_ORG_SPACE=staging" >> $GITHUB_ENV
+    echo "CF_ORG_SPACE=staging"
+    ;;
   (stg-v[0-9]*.[0-9]*)
     echo "CF_ORG_SPACE=staging" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=staging"
