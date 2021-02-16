082a64937d7671d01c579ecb01e2f5a2b512d238
Srinivasa Regeti <regeti@regeti.com>
2021-02-16 18:40:02 -0500

    Commented out the lines to speedup the process

--- scripts/zap-api-scan.sh
+++ scripts/zap-api-scan.sh
@@ -2,30 +2,30 @@
 
 cd $GITHUB_WORKSPACE
 
-cp devops/api-zap-scan.conf devops/scripts/
-cd devops/scripts/
+# cp devops/api-zap-scan.conf devops/scripts/
+# cd devops/scripts/
 
 # docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -c api-zap-scan.conf -r report.html
 
-sudo mkdir /zap/wrk
-sudo chmod 777 /zap/wrk
+# sudo mkdir /zap/wrk
+# sudo chmod 777 /zap/wrk
 
-sudo docker run --name dv_git_workspace -v /zap/wrk gliderlabs/alpine:latest /bin/true
+# sudo docker run --name dv_git_workspace -v /zap/wrk gliderlabs/alpine:latest /bin/true
 
-sudo docker run -t --volumes-from dv_git_workspace owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi -d  -g zap.conf
+# sudo docker run -t --volumes-from dv_git_workspace owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi -d  -g zap.conf
 
 echo "DEBUG: Files in Base directory"
 
-docker volume ls
+# docker volume ls
 
-volumeName=$(docker volume ls --format "{{.Name}}")
+# volumeName=$(docker volume ls --format "{{.Name}}")
 
 
-sudo ls -lR /var/lib/docker/volumes/$volumeName
+# sudo ls -lR /var/lib/docker/volumes/$volumeName
 
-sudo find /var/lib/docker/volumes/$volumeName/ -name zap.conf -print
+# sudo find /var/lib/docker/volumes/$volumeName/ -name zap.conf -print
 
-sudo find /var/lib -name zap.conf -print 
+sudo find $GITHUB_WORKSPACE  -name zap-api-scan.conf -print 
 
 
 # aws s3 ls s3://$ARTIFACTS_STORAGE/
