5588929a62ec51b4dd1955addb8a51587f7811ad
Srinivasa R Regeti <regeti@regeti.com>
2021-02-16 16:00:16 -0500

    Added debug statement

--- scripts/zap-api-scan.sh
+++ scripts/zap-api-scan.sh
@@ -7,6 +7,8 @@ cd devops/scripts/
 
 # docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -c api-zap-scan.conf -r report.html
 
+sudo mkdir /zap/wrk
+
 sudo docker run --name dv_git_workspace -v /zap/wrk gliderlabs/alpine:latest /bin/true
 
 sudo docker run -t --volumes-from dv_git_workspace -v /var/run/docker.sock:/var/run/docker.sock owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi -d  -g zap.conf
