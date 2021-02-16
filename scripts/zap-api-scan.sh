bd011a1475a46da59c82d6fbfd3889cbd6d1fa98
Srinivasa R Regeti <regeti@regeti.com>
2021-02-16 12:35:25 -0500

    Updated zap scan script with s3 copy

--- scripts/zap-api-scan.sh
+++ scripts/zap-api-scan.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi
+cp devops/api-zap-scan.conf . 
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi -j -c api-zap-scan.conf -r report-$GITHUB_RUN_NUMBER.html
+
+aws s3 cp report-$GITHUB_RUN_NUMBER.html s3://$ARTIFACTS_STORAGE/zap-scan-reports/
+aws s3 ls s3://$ARTIFACTS_STORAGE/zap-scan-reports/
+
