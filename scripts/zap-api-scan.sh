3843b36fc8911531ec733895f10af076a17a0cf4
Srinivasa R Regeti <regeti@regeti.com>
2021-02-16 14:58:28 -0500

    Added debug statement

--- scripts/zap-api-scan.sh
+++ scripts/zap-api-scan.sh
@@ -7,7 +7,10 @@ cd devops/scripts/
 
 # docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -c api-zap-scan.conf -r report.html
 
-docker run -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -g zap.conf
+echo "DEBUG: List of all Volumes"
+docker volume ls
+
+# docker run -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -g zap.conf
 
 echo "DEBUG: Files in Base directory"
 
@@ -17,7 +20,7 @@ ls -ltr /__w/
 ls -ltr zap.conf
 
 
-aws s3 ls s3://$ARTIFACTS_STORAGE/
+# aws s3 ls s3://$ARTIFACTS_STORAGE/
 
 # aws s3 cp report-$GITHUB_RUN_NUMBER.html s3://$ARTIFACTS_STORAGE/zap-scan-reports/
 # aws s3 ls s3://$ARTIFACTS_STORAGE/zap-scan-reports/
