1456cbe891b7d2d494d7e626412c8453f0a2d743
Srinivasa R Regeti <regeti@regeti.com>
2021-02-16 14:08:27 -0500

    Added debug statement

--- scripts/zap-api-scan.sh
+++ scripts/zap-api-scan.sh
@@ -5,7 +5,9 @@ cd $GITHUB_WORKSPACE
 cp devops/api-zap-scan.conf devops/scripts/
 cd devops/scripts/
 
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -c api-zap-scan.conf -r report.html
+# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -c api-zap-scan.conf -r report.html
+
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-api-scan.py -t https://easey-dev.app.cloud.gov/api/facility-mgmt/swagger -f openapi  -g zap.conf
 
 echo "DEBUG: Files in Base directory"
 
