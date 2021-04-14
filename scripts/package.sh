42198210d2a21f78ff9edf8fae7b361039b6f99b
jasonwhitehead <jasonwhitehad@cvpcorp.com>
2021-04-14 11:06:54 -0500

    modifications to package script to include /dist folder

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,17 +6,15 @@ echo "Retrieving app, version, and build #..."
 echo "App Name: $APP_NAME"
 echo "App Version: $APP_VERSION"
 echo "Build #: $GITHUB_RUN_NUMBER"
-
-echo ""
-echo "Package contents..."
-ls -l
 echo ""
 
 echo "Building Artifact: $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip"
 cd ../
-zip -q -x \*.git* -r $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip `basename $GITHUB_WORKSPACE`
-ls -lh $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip
+zip -q -r $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip `basename $GITHUB_WORKSPACE` -x */\.* *.git* \.* *.md *Docker* *docker* *manifest* LICENSE /node_modules/* /test/*
+ls -l $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip
+echo ""
 
 echo "Copying package to deployment artifacts..."
 aws s3 cp $APP_NAME.$APP_VERSION.$GITHUB_RUN_NUMBER.zip s3://$ARTIFACTS_STORAGE/
 aws s3 ls s3://$ARTIFACTS_STORAGE/$APP_NAME.$APP_VERSION
+echo ""
