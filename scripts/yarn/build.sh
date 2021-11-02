c717823077589ca65caa31e16820b05c2b27ca26
Jason Whitehead <jwhitehead@austin.rr.com>
2021-11-01 21:47:18 -0500

    working on yarn install

--- scripts/yarn/build.sh
+++ scripts/yarn/build.sh
@@ -4,16 +4,8 @@ echo "--------------------------------------"
 echo "----------- yarn/build.sh ------------"
 echo "--------------------------------------"
 
-#echo "Installing dependencies..."
-#yarn install
-
-#echo "Creating npm-packages-offline-cache..."
-#yarn config set yarn-offline-mirror ./npm-packages-offline-cache
-#yarn config set yarn-offline-mirror-pruning true
-#rm -rf node_modules/ yarn.lock
-
 echo "Installing dependencies..."
-yarn install
+yarn install --ignore-engines
 
 echo "Building..."
 CI=false yarn build
