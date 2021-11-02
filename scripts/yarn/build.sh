b014cb6027ea1b5378ad4dd86be32f85d1a94209
Jason Whitehead <jwhitehead@austin.rr.com>
2021-11-02 10:05:22 -0500

    configuration of yarn offline npm package cache

--- scripts/yarn/build.sh
+++ scripts/yarn/build.sh
@@ -4,6 +4,10 @@ echo "--------------------------------------"
 echo "----------- yarn/build.sh ------------"
 echo "--------------------------------------"
 
+echo "Configuring Yarn Offline NPM Package Cache..."
+echo 'yarn-offline-mirror "./npm-packages-offline-cache"' >> .yarnrc
+echo 'yarn-offline-mirror-pruning true' >> .yarnrc
+
 echo "Installing dependencies..."
 yarn install --ignore-engines
 
