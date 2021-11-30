13b1acb82764d112b510ba533bf3a9dd25ab6747
Jason Whitehead <jwhitehead@austin.rr.com>
2021-11-30 16:36:50 -0600

    adding some logging

--- scripts/cf-app-init.sh
+++ scripts/cf-app-init.sh
@@ -8,4 +8,6 @@ if [[ $(cf app $APP_NAME) == *"FAILED"* ]]
 then
   echo "$APP_NAME application does not exist! Creating application shell..."
   cf push $APP_NAME --no-manifest --no-route --no-start
+else
+  echo "$APP_NAME application exists! Skipping application initialization..."
 fi
