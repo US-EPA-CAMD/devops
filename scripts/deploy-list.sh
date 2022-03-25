692ba1b390e9f84c49b05f2996cc489328045171
ACPR <yefimabramson@cvpcorp.com>
2022-03-25 12:07:51 -0400

    updated app versions for prod deployment

--- scripts/deploy-list.sh
+++ scripts/deploy-list.sh
@@ -1,7 +1,7 @@
-//EASEY v1.0.21 account-api
-//EASEY v1.0.25 emissions-api
-//EASEY v1.0.43 facilities-api
-//EASEY v1.0.24 mdm-api
-//EASEY v1.0.2 notification-api
+EASEY v1.0.24 account-api
+EASEY v1.0.30 emissions-api
+EASEY v1.0.46 facilities-api
+EASEY v1.0.24 mdm-api
+EASEY v1.0.2 notification-api
 EASEY v1.0.4 ssh-tunnel
-//REACT_APP_EASEY v1.0.31 campd-ui
+REACT_APP_EASEY v1.0.31 campd-ui
