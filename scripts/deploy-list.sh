e56b2181ed97a457dfdd1346c467abd2dd6f39e6
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2022-04-28 12:55:36 -0500

    Update deploy-list.sh
    updating to latest versions in staging that were approved for prod release

--- scripts/deploy-list.sh
+++ scripts/deploy-list.sh
@@ -1,7 +1,5 @@
-EASEY v1.0.24 account-api
-EASEY v1.0.30 emissions-api
-EASEY v1.0.46 facilities-api
 EASEY v1.0.24 mdm-api
-EASEY v1.0.2 notification-api
-EASEY v1.0.4 ssh-tunnel
-REACT_APP_EASEY v1.0.31 campd-ui
+EASEY v1.0.27 account-api
+EASEY v1.0.34 emissions-api
+EASEY v1.0.49 facilities-api
+REACT_APP_EASEY v1.0.35 campd-ui
