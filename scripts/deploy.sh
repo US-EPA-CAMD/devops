5a645cbb63d2526c1a9d8f541355205785531862
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2022-06-23 22:52:37 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,6 @@ echo "--------------------------------------"
 echo "------------- deploy.sh --------------"
 echo "--------------------------------------"
 
-chmod -R 600
-
 if [[ $(cf app $APP_NAME) == *"FAILED"* ]]
 then
   echo "$APP_NAME application does not exist! Creating application shell..."
