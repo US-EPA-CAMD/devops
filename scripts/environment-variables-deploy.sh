835f870a45be959347e7305675d5ab5b56a4a503
ACPR <yefimabramson@cvpcorp.com>
2022-08-03 12:36:53 -0400

    added OWASP ZAP Scan file

--- scripts/environment-variables-deploy.sh
+++ scripts/environment-variables-deploy.sh
@@ -28,26 +28,55 @@ echo "Package: $PACKAGE"
 echo ""
 
 case $ENV in
-  (CDC)
-    echo "CF_ORG_SPACE=cdc" >> $GITHUB_ENV
-    echo "CF_ORG_SPACE=cdc"
-    ;;
+  # (CDC)
+  #   echo "CF_ORG_SPACE=cdc" >> $GITHUB_ENV
+  #   echo "CF_ORG_SPACE=cdc"
+  #   ;;
   (Beta)
     echo "CF_ORG_SPACE=beta" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=beta"
+
+    echo "URL=$URL+beta/" >> $GITHUB_ENV
+
+    echo "CF_ENV_DEPLOYMENT_SVC=$CF_BETA_DEPLOYMENT_SVC" >> $GITHUB_ENV
+    echo "CF_ENV_DEPLOYMENT_SVC_PWD=$CF_BETA_DEPLOYMENT_SVC_PWD" >> $GITHUB_ENV
     ;;
   (Test)
     echo "CF_ORG_SPACE=test" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=test"
+
+    echo "URL=$URL+test/" >> $GITHUB_ENV
+
+    echo "CF_ENV_DEPLOYMENT_SVC=$CF_TST_DEPLOYMENT_SVC" >> $GITHUB_ENV
+    echo "CF_ENV_DEPLOYMENT_SVC_PWD=$CF_TST_DEPLOYMENT_SVC_PWD" >> $GITHUB_ENV
     ;;
   (Staging)
     echo "CF_ORG_SPACE=staging" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=staging"
+
+    echo "URL=$URL+staging/" >> $GITHUB_ENV
+
+    echo "CF_ENV_DEPLOYMENT_SVC=$CF_STG_DEPLOYMENT_SVC" >> $GITHUB_ENV
+    echo "CF_ENV_DEPLOYMENT_SVC_PWD=$CF_STG_DEPLOYMENT_SVC_PWD" >> $GITHUB_ENV
     ;;
+  (Performance)
+    echo "CF_ORG_SPACE=perf" >> $GITHUB_ENV
+    echo "CF_ORG_SPACE=perf"
+
+    echo "URL=$URL+performance/" >> $GITHUB_ENV
+
+    echo "CF_ENV_DEPLOYMENT_SVC=$CF_PERF_DEPLOYMENT_SVC" >> $GITHUB_ENV
+    echo "CF_ENV_DEPLOYMENT_SVC_PWD=$CF_PERF_DEPLOYMENT_SVC_PWD" >> $GITHUB_ENV
+    ;;    
   # This is to match case of no tag for dev env as we do not want a malformed tag pushing to dev
   "")
     echo "CF_ORG_SPACE=dev" >> $GITHUB_ENV
     echo "CF_ORG_SPACE=dev"
+
+    echo "URL=$URL+dev/" >> $GITHUB_ENV
+
+    echo "CF_ENV_DEPLOYMENT_SVC=$CF_DEV_DEPLOYMENT_SVC" >> $GITHUB_ENV
+    echo "CF_ENV_DEPLOYMENT_SVC_PWD=$CF_DEV_DEPLOYMENT_SVC_PWD" >> $GITHUB_ENV
     ;;
   # if nothing matches we need to error and exit since the CF_ORG_SPACE is not properly set
   *)
@@ -55,3 +84,48 @@ case $ENV in
     exit 1
     ;;
 esac
+
+api=true
+
+case $GUTHUB_REPOSITORY in
+  (US-EPA-CAMD/easey-campd-ui)
+    echo "URL=$URL+repo/" >> $GITHUB_ENV
+    api=false
+    ;;
+  (US-EPA-CAMD/easey-ecmps-ui)
+    echo "URL=$URL+repo/" >> $GITHUB_ENV
+    api=false
+    ;;
+  (US-EPA-CAMD/easey-account-api)
+    echo "URL=$URL+account-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-auth-api)
+    echo "URL=$URL+auth-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-camd-services)
+    echo "URL=$URL+camd-services/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-emissions-api)
+    echo "URL=$URL+emissions-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-facilities-api)
+    echo "URL=$URL+facilities-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-mdm-api)
+    echo "URL=$URL+master-data-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-monitor-plan-api)
+    echo "URL=$URL+monitor-plan-mgmt/" >> $GITHUB_ENV
+    ;;
+  (US-EPA-CAMD/easey-qa-certifications-api)
+    echo "URL=$URL+qa-certifications-mgmt/" >> $GITHUB_ENV
+    ;;
+  *)
+    echo "repo not set!! please edit environment-variables-deploy.sh"
+    ;;
+esac
+
+if api; then
+  echo "URL=$URL+swagger/" >> $GITHUB_ENV
+  echo "URL=$URL+swagger/"
+fi
