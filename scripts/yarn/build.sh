673bb194b54f142688bae18a99b3ec609193342f
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2023-01-17 16:52:49 -0600

    Update build.sh

--- scripts/yarn/build.sh
+++ scripts/yarn/build.sh
@@ -5,9 +5,7 @@ echo "----------- yarn/build.sh ------------"
 echo "--------------------------------------"
 
 echo "Configuring Yarn Offline NPM Package Cache..."
-#echo 'yarn-offline-mirror "./npm-packages-offline-cache"' >> .yarnrc
 yarn config set yarn-offline-mirror ./npm-packages-offline-cache
-#echo 'yarn-offline-mirror-pruning true' >> .yarnrc
 yarn config set yarn-offline-mirror-pruning true
 
 echo "Installing dependencies..."
