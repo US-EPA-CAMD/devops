f0766a32daa0104fd1acb3551d446b1d789eb5c7
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2023-02-01 10:12:00 -0600

    Update build.sh

--- scripts/yarn/build.sh
+++ scripts/yarn/build.sh
@@ -5,11 +5,16 @@ echo "----------- yarn/build.sh ------------"
 echo "--------------------------------------"
 
 echo "Configuring Yarn Offline NPM Package Cache..."
+yarn config set yarn-offline-mirror ./npm-packages-offline-cache
 echo 'yarn-offline-mirror "./npm-packages-offline-cache"' >> .yarnrc
+yarn config set yarn-offline-mirror-pruning true
 echo 'yarn-offline-mirror-pruning true' >> .yarnrc
 
 echo "Installing dependencies..."
+rm -rf node_modules/
 yarn install --ignore-engines
 
 echo "Building..."
 CI=false yarn build
+
+ls -al
