bf305595223023baad69cdb2a949edb9e5dd314b
Jason Whitehead <66504934+jwhitehead77@users.noreply.github.com>
2023-03-28 09:58:59 -0500

    Update install-cf-cli.sh

--- scripts/install-cf-cli.sh
+++ scripts/install-cf-cli.sh
@@ -5,7 +5,7 @@ echo "--------------------------------------"
 echo "--------- install-cf-cli.sh ----------"
 echo "--------------------------------------"
 
-curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github&version=v6" | tar -zx
+curl -A "" -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github&version=v6" | tar -zx
 sudo mv cf /usr/local/bin
 sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli-ci/master/ci/installers/completion/cf
 cf version
