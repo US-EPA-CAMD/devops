ca806999f62635dc33fa6aaa2ccb3c3e3c6ef685
maheese <heese.michael@epa.gov>
2024-10-15 10:22:50 -0400

    Added hook to set scan strength

---
+++ scan-hooks/api_scan.py
@@ -0,0 +1,7 @@
+# Description: This file contains the API calls for the scan hooks.  
+# This hook sets the scan policy strength to 'high' and threshold to 'low'.
+# This active scan settings matches the what is used by the NCC
+# Note: this scan will take longer to complete and should therefore be run at off-peak times.
+
+def zap_active_scan(zap, target, policy):
+    return zap, target, 'St-High-Th-Low'
