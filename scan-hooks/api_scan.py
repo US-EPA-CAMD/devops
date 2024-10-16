4ed405b110d5e3977ab779fdbf2fc095a3446eb8
maheese <heese.michael@epa.gov>
2024-10-16 11:49:19 -0400

    Modified to set policy

--- scan-hooks/api_scan.py
+++ scan-hooks/api_scan.py
@@ -2,10 +2,13 @@
 # This hook sets the scan policy strength to 'high' and threshold to 'low'.
 # This active scan settings matches the what is used by the NCC
 # Note: this scan will take longer to complete and should therefore be run at off-peak times.
+import os
+
 
 def zap_active_scan(zap, target, policy):
     # Log the incoming parameters
     print('Incoming policy: ' + str(policy))
-    policy = 'St-Ins-Th-Low.policy'
+    # Read the zap scan policy from an environment variable
+    policy = os.getenv('ZAP_SCAN_POLICY')
     print('Outgoing policy: ' + str(policy))
     return zap, target, policy
